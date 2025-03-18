dc94fc3b2247dba6dec9e4db760b8317da6bbe79
Rohit Ghodke <rohit.ghodke@oracle.com>
2025-03-18 05:02:16 +0000

    added first.py

---
+++ first.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+''' Author:rohit
+  version:0.1
+  Desc: This is the first script'''
+
+print("Hello world")
+
+name = "First Last"
+print("My name is", name)
+print("My name is " + name)
