21b5f27effdc66d9fbd501d33d9f621de158966c
Rohit Ghodke <rohit.ghodke@oracle.com>
2025-03-18 12:45:39 +0000

    added ex2, ex2_2

---
+++ rohit_demos/ex2.py
@@ -0,0 +1,11 @@
+fname="Rohit"
+lname="Ghodke"
+
+print(fname, lname)
+
+list_name=list[fname,lname]
+print(list_name)
+
+dict_name={fname:'Rohit', lname:'Ghodke'}
+print(dict_name)
+
---
+++ rohit_demos/ex2_2.py
@@ -0,0 +1,10 @@
+
+var=input("Please enter a value:")
+
+var_upper=var.upper()
+print(var_upper)
+
+var_len=len(var)
+print(var_len)
+
+print(var.isdecimal())
